0ef66bcc0cd156ff5b73f1e4e4160cd2f1c367fa
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-13 07:16:43 +0700

    x

--- src/AppToDoList.js
+++ src/AppToDoList.js
@@ -9,24 +9,55 @@ const initialJobs = [
 const App = () => {
 
     const [job, setJob] = useState('');
+    const [editJob, setEditJob] = useState('');
     const [listJobs, setListJobs] = useState(initialJobs);
+    const [isEdit, setIsEdit] = useState(false);
 
     const handleAdd = () => {
-        let newArray = [...listJobs];
-        newArray.push(job);
-        setListJobs(newArray);
-        setJob('');
+        if (!isEdit) {
+            let newArray = [...listJobs];
+            newArray.push(job);
+            setListJobs(newArray);
+            setJob('');
+        } else {
+            let newArray = [...listJobs];
+            let idx = newArray.findIndex(jb => jb === editJob);
+            newArray.splice(idx, 1, job);
+            setListJobs(newArray);
+            setJob('');
+            setIsEdit(false);
+        }
+    }
+
+    const handleClickEdit = (job) => {
+        setJob(job);
+        setEditJob(job);
+        // console.log(editJob);
+        setIsEdit(true);
+    }
+
+    const handleDelete = (job) => {
+        if (confirm('Bạn có chắc muốn xóa ?')) {
+            let newArray = [...listJobs];
+            let idx = newArray.findIndex(jb => jb === job);
+            newArray.splice(idx, 1);
+            setListJobs(newArray);
+        }
     }
 
     return (
         <>
             <input type='text' value={job}
                 onChange={(e) => setJob(e.target.value)} />
-            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleAdd}>{isEdit ? 'Edit' : 'Add'}</button>
 
             <ul>
                 {listJobs.map((job, idx) =>
-                    <li key={idx}>{job}</li>
+                    <li key={idx}>
+                        <p>{job}</p>
+                        <button onClick={() => handleClickEdit(job)}>Sửa</button>
+                        <button onClick={() => handleDelete(job)}>Xóa</button>
+                    </li>
                 )}
             </ul>
         </>
